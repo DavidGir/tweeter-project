0e9724a85540807708143968603524640220e284
David Giroux <DavidGiroux149@gmail.com>
2023-11-07 16:37:02 -0500

    add setTimeout to give immersion access mainframe

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,17 @@
 // Implementing event listener for when user accesses textarea "the mainframe" with the combination of setTImeout to give that extra immersion:
 $(document).ready(function() {
   $("#tweet-text").focus(function() {
-    console.log("Neural Link Established ... Broadcast booting ... Welcome to subnet");
+    // 500ms delay
+    setTimeout(function() {
+      console.log("Neural Link Established...");
+    }, 500);
+    // Then wait another 1000ms before this logs
+    setTimeout(function() {
+      console.log("Broadcast booting...");
+    }, 1500);
+    // Then wait another 1000ms before this logs
+    setTimeout(function() {
+      console.log("Welcome to subnet");
+    }, 2500);
   });
 });
