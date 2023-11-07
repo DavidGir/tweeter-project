030130c08d689068acad34843c20e7115b65a776
David Giroux <DavidGiroux149@gmail.com>
2023-11-07 18:31:20 -0500

    add event input; captures changes made in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
+// Targeting textarea by its ID:
 $(document).ready(function() {
-  console.log("Crimson Script");
+  $("#tweet-text").on("input", function() {
+    // "This" refers to the textarea DOM element
+    // Convert "This" to a jQuery object to use jQuery methods like .val()
+    const textLength = $(this).val().length;
+    const charactersLeft = 140 - textLength;
+
+    console.log(charactersLeft);
+  });
 });
+
