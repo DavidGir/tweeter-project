ae32c5b62617da994c54eec2614c0fba839d4c93
David Giroux <DavidGiroux149@gmail.com>
2023-11-07 16:34:08 -0500

    use action event listener upon textarea focus

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Implementing event listener for when user accesses textarea "the mainframe" with the combination of setTImeout to give that extra immersion:
+$(document).ready(function() {
+  $("#tweet-text").focus(function() {
+    console.log("Neural Link Established ... Broadcast booting ... Welcome to subnet");
+  });
+});
