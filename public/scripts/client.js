0a8d01bc90dffaa19dc3884009f805d48ecb7007
David Giroux <DavidGiroux149@gmail.com>
2023-11-07 16:52:30 -0500

    add setInterval loading dots sequence

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,17 +7,36 @@
 // Implementing event listener for when user accesses textarea "the mainframe" with the combination of setTImeout to give that extra immersion:
 $(document).ready(function() {
   $("#tweet-text").focus(function() {
+    // Start loading sequence; adding dots every 500ms
+    let loadingDots = setInterval(function() {
+      console.log('......');
+    }, 500);
+    
     // 500ms delay
     setTimeout(function() {
-      console.log("Neural Link Established...");
+      clearInterval(loadingDots); // Stop adding dots
+      console.log("Neural Link Established");
     }, 500);
+
+    loadingDots = setInterval(function() {
+      console.log('......');
+    }, 500);
+
     // Then wait another 1000ms before this logs
     setTimeout(function() {
-      console.log("Broadcast booting...");
+      clearInterval(loadingDots);
+      console.log("Broadcast booting");
+    
+      loadingDots = setInterval(function() {
+        console.log('......');
+      }, 500);
+
+      // Then wait another 1000ms before this logs
+      setTimeout(function() {
+        clearInterval(loadingDots);
+        console.log("Welcome to subnet");
+      }, 1500);
+      // After 1500ms from the initial message
     }, 1500);
-    // Then wait another 1000ms before this logs
-    setTimeout(function() {
-      console.log("Welcome to subnet");
-    }, 2500);
   });
 });
