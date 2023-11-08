e762caa391716ba6be70c9b7f6ba38508a6c8b7a
David Giroux <DavidGiroux149@gmail.com>
2023-11-07 19:09:20 -0500

    add edge-case to adjust counter color if invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,6 +12,14 @@ $(document).ready(function() {
     let counter = $(this).closest("form").find(".counter");
     // Add text setter; it will set text content to update the displayed count
     counter.text(charactersLeft);
+
+    // Adjust the counter color based on validity (no more characters)
+    if (charactersLeft < 0) {
+      // Add class dynamically on browser html and not on the source code:
+      counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid");
+    }
   });
 });
 
