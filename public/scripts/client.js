6cc7e2b7533dedcabc81c8d1f431945cba9ca65d
David Giroux <DavidGiroux149@gmail.com>
2023-11-08 18:50:31 -0500

    add fake data and create renderTweets func

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -46,6 +46,45 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+  
+  // Fake data from the initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  
+  // Responsible for taking in an array of tweet objects and then appending each one to the #tweets-container
+  const renderTweets = function(arrayOfTweetObjects) {
+    // Loop through tweets
+    for (const tweet of arrayOfTweetObjects) {
+      // Call createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // Append return value to the tweets container
+      $("#tweets-container").append($tweet);
+    }
+  };
+
+  // Takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
   const createTweetElement = function(tweetObject) {
     const $tweet = $(`
       <article class="tweet">
@@ -70,23 +109,8 @@ $(document).ready(function() {
       `);
     return $tweet;
   };
+  renderTweets(data);
 
-  // Test code (temporary)
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  //
 });
+
